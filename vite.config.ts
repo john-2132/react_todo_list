6597a0c6704bf7c38294f5b9e09d3d269dea07ce
john-2132 <junichi.k0211@gmail.com>
2025-05-19 23:29:43 +0900

    viteのbase設定を修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES
-        ? "react-todo"
-        : "./", 
+  base: '/react-todo/',
   plugins: [react()],
 })
